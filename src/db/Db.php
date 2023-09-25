7606dc606f69c893ec8838e841a74aa6b8f5bff0
bobo137 <pierre.calves13@gmail.com>
2023-09-25 17:12:21 +0200

    heroku db

--- src/db/Db.php
+++ src/db/Db.php
@@ -8,10 +8,10 @@ use PDOException;
 class Db extends PDO
 {
   private static $instance;
-  private const DBHOST = 'localhost';
-  private const DBUSER = 'root';
-  private const DBPASS = '';
-  private const DBNAME = 'garagevpdb';
+  private const DBHOST = 'ec2-18-205-44-21.compute-1.amazonaws.com';
+  private const DBUSER = 'wznxnggnfgjycx';
+  private const DBPASS = '719459fe0bdeb2234e1e577c8d1ef8bb2185cee57993764919785db8ae067c71';
+  private const DBNAME = 'd8nt1ild55488q';
 
   private function __construct()
   {
